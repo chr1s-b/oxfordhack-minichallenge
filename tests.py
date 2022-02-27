082841ef0f52fff5c5a1732f7ca4424c4d608120
ninjutsufish <formaggio@hotmail.co.uk>
2022-02-27 08:17:18 +0000

    added test for mobile compatabitlity

--- tests.py
+++ tests.py
@@ -1,6 +1,8 @@
 import os
 from dotenv import load_dotenv
 import googlemaps
+import requests
+import json
 from datetime import datetime
 
 # load in api key for Google Places API
@@ -11,7 +13,19 @@ print(f"Loaded Google Places API key {API_KEY}")
 gmaps = googlemaps.Client(key=API_KEY)
 
 def mobilecompatibility(url):
-    return 0,{}
+    """uses google mobile support checker webpage to see if the webpage has mobile support """
+    apiUrl = 'https://searchconsole.googleapis.com/v1/urlTestingTools/mobileFriendlyTest:run'
+    params = {
+            'url': url,
+            'requestScreenshot': 'false',
+            'key': "AIzaSyB4hW39-INaUINhqCeaRXkMx87KJGCAYIY"
+        }
+
+    x = requests.post(apiUrl, data = params)
+    data = json.loads(x.text)
+    print(data['test_status'] == {'status': 'COMPLETE'})
+    return data['mobileFriendliness'] == 'MOBILE_FRIENDLY'
+
 
 
 def gmapspresence(business):
